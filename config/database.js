3b27b3eed8ea890206b27d48e9c9434144533df1
MysticUnios1998 <eduard.arias@mail.escuelaing.edu.co>
2021-11-23 19:08:01 -0500

    Reposirotio de datos actualizado y mejorado

--- config/database.js
+++ config/database.js
@@ -12,19 +12,18 @@ let db = new sqlite.Database(DBSOURCE, (err) => {
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title text, 
             description text, 
-            dueDate numeric,
+            dueDate text,
             assignedTo text,
-            status numeric,
+            status text,
             leftColor text,
             rightColor text
             )`,
         (err) => {
-            if (!err) {
-                // Table just created, creating some rows
-                var insert = 'INSERT INTO tasks (title, description, dueDate, assignedTo, status, leftColor, rightColor) VALUES (?,?,?,?,?,?,?)'
-                //db.run(insert, ["Technique Guides","Learn amazing street workout and calisthenics","https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06","#A74CF2","#617BFB"])
-            }
+            return;
         });
+        
+        var insert = 'INSERT INTO tasks (title, description, dueDate, assignedTo, status, leftColor, rightColor) VALUES (?,?,?,?,?,?,?)'
+        db.run(insert, ["Tarea Test Hoho", "Este es un intento", "14/10/2021", "Felipe Marin", "EN PROCESO", "#A74CF2", "#617BFB"])
     }
 })
 
